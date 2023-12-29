62e88f7e6f2cbd9e7d40226b2a813e9ec7b34eef
Aadhitya A <aadhitya864@gmail.com>
2023-12-29 22:10:56 +0530

    Init terma
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Terma
+
+Terma is a terminal style portfolio website. _Work in progress_
+
+## Libraries Used
+
+jQuery, JS
+
+## License
+
+MIT License
---
+++ js/main.js
@@ -0,0 +1,211 @@
+var API_PREFIX = 'https://api.github.com/repos/alphax86/terma/git',
+    e = "visitor@axro";
+var GitHub = new (function() {
+    this.fs = new Object;
+    this.loaded = false;
+    this.stack = new Array;
+
+    this.getCurrentPath = function(){
+        if(this.stack.length == 0) 
+            return '~/';
+        return this.stack.join('/')
+    },    
+    this.getCurrentWorkingDirectory = function() {
+        if(this.stack.length == 0) 
+            return this.fs;
+        
+        var fs = this.fs
+        for(var i in this.stack) {
+            fs = fs[this.stack[i]];
+        }
+        return fs;
+    };
+        
+    var self = this;    
+    $.getJSON(API_PREFIX + '/refs/heads/content', function(data, textStatus, jqXHR){
+    //$.getJSON('data/master.json', function(data, textStatus, jqXHR){
+        var sha = data.object.sha;
+        $.getJSON(API_PREFIX + '/trees/'+sha+'?recursive=1', function(data, textStatus, jqXHR){
+        //$.getJSON('data/tree.json', function(data, textStatus, jqXHR){
+            for(i in data.tree) {
+                var item = data.tree[i];                
+                var paths = item.path.split('/');   
+                
+                var fs = self.fs;                
+                for(var i=0; i< paths.length; i++) {
+                    var path = paths[i];                    
+                    
+                    if(!fs.hasOwnProperty(path)) {
+                       fs[path] = new Object;
+                    } else {
+                       fs = fs[path]
+                    }
+                       
+                    if (i == paths.length-1) {
+                        item.path = path;
+                        fs[path] = item;
+                    }
+                }
+            }
+            self.loaded = true;
+        });
+    });
+})();
+
+var App = {
+    echo: function(text) {
+        this.echo(text);
+    },
+    help: function() {
+        this.echo("Available commands:");
+        this.echo("\tabout       information about the author");
+        this.echo("\tcontact     display contact infomation");
+        this.echo("\tprojects    display project list");
+        this.echo("\tskill       display skill tier list");
+        this.echo("\twhoami      display who the user is");
+        this.echo("\tmeow        don't! just don't! (╯°□°）╯︵ ┻━┻");
+        this.echo("\tdank        you know what it is ( ͡° ͜ʖ ͡°) also, it's a meme. Don't take it seriously.");
+        this.echo("\thelp        this help screen.");                        
+        this.echo("");
+        this.echo("some other basic Linux commands are available: cat cd id ls sudo")
+    },
+    about: function() {
+        this.echo("Hello, my name is Aadhitya, I'm from Chennai, India.");
+        this.echo("I'm a software developer who really loves Open Source and passionate to create, contrinute to Open Source projects. Currently I work at Citi as a SDE.");
+        this.echo("My technical summary:");
+        this.echo("\t- Have strong knowledge about Linux operating system and open source software.");
+        this.echo("\t- Responsible for day-to-day defense of our network, servers.");
+        this.echo("\t- Experienced with web application development, specialist with PHP/MySQL. Can develop desktop/web application with Java/Python.");
+        this.echo("\t- Able to make mobile apps using many different technologies (Native/Titanium/PhoneGap)");
+        this.echo("\nIf you want to say hi or anything, feel free to get in touch via [[b;#44D544;]contact] command");
+
+    },
+    contact: function() {
+        this.echo("Get in touch via:")
+        this.echo("Email:   " + decode64('YWFkaGl0eWE4NjRAcHJvdG9uLm1l')); 
+        this.echo("Twitter: @kryox64"); 
+        this.echo("GitHub:  alphax86"); 
+    },
+    whoami: function() {
+        this.echo("visitor", {raw:true});
+
+        
+    },
+    id: function(){
+        this.echo("uid=1000(visitor) gid=1000(visitor) groups=1000(visitor)");
+
+        
+    },
+    ls: function() {        
+        var wd = GitHub.getCurrentWorkingDirectory();
+        for(i in wd) {
+            if(typeof wd[i] == 'object') {
+                var item = wd[i];
+                this.echo(item.mode+'\t' + (item.type=='tree'?'[[b;#44D544;]'+item.path+']':item.path));
+            }
+        }
+
+        
+    },
+    cd: function(path) {        
+        if(path == '..') {
+            GitHub.stack.pop();
+            return;
+        }        
+        var wd = GitHub.getCurrentWorkingDirectory();
+        var item = wd[path]
+        if(!item) {
+            this.error("cd: " + path + ": No such file or directory");
+        } else if(item.type != 'tree') {
+            this.error("cd: " + path  + ": Not a directory");
+        } else {
+            GitHub.stack.push(path);
+        }
+
+        
+    },
+    cat: function(path){
+        var wd = GitHub.getCurrentWorkingDirectory();
+        var item = wd[path];
+        if(!item) {
+            this.error("cat: " + path + ": No such file or directory");
+        } else if(item.type == 'tree') {
+            this.error("cat: " + path  + ": Is a directory");
+        } else {
+            var term = this;
+            term.pause();
+            $.getJSON(item.url, function(data, textStatus, jqXHR){
+                var content = data.content.trim()
+                if(data.encoding == 'base64')
+                    content = decode64(content);
+                term.echo(content); 
+                term.resume();
+            });
+        }
+        
+    },
+    sudo: function() {
+        this.echo("Sorry, I can't do that. You are not in the sudoers file. This incident will be reported. ￣へ￣");
+    },
+    skill: function() {
+        this.echo("Skill tier list:");
+        this.echo("\t- [[b;#44D544;]Master] - C/C++, Python, Git");
+        this.echo("\t- [[b;#44D544;]Intermediate] - Java, JS, Go, Docker, Linux, MySQL, MongoDB");
+        this.echo("\t- [[b;#44D544;]Beginner] - Kafka, Spark, AWS, Kubernetes");
+    },
+    projects: function() {
+        this.echo("Project list:");
+        this.echo("\t- [[b;#44D544;]terma]      - this page");
+        this.echo("\t- [[b;#44D544;]CreDeX]     - https://github.com/alphaX86/neueda-credit");
+        this.echo("\t- [[b;#44D544;]arkLB]      - https://github.com/ark-7/arkLB");
+        this.echo("\t- [[b;#44D544;]HexE]       - https://github.com/HexE-r/HexE");
+        this.echo("\t- [[b;#44D544;]accentVGG]  - https://github.com/k173-x/accent-vgg");
+        this.echo("\t- [[b;#44D544;]meshery]    - https://github.com/meshery/meshery");
+    },
+    meow: function() {
+        this.echo("Byeeeeeee ┬─┬ノ( º _ ºノ)");
+        this.echo("Just kidding, I'm not going anywhere. Not yet...");
+        //window.location.href = "https://axro.in";
+    },
+    dank: function() {
+        var term = this;
+        term.pause();
+        $.getJSON('https://meme-api.com/gimme/dankmemes', function(data){
+            var meme = data.preview[3];
+            term.echo("Here's a dank meme for you ( ͡° ͜ʖ ͡°)");
+            if (meme == null || meme == undefined) {
+                term.echo("Sorry, I can't find any dank memes for you. Try again later.");
+            } else {
+                term.echo(meme);
+            }
+            term.resume();
+        });
+    },
+}
+
+jQuery(document).ready(function($) {
+    $('body').terminal(App, {
+        greetings: "[[b;#44D544;]      _       ____  ____  _______      ___    \n" +
+            "     / \\     |_  _||_  _||_   __ \\   .'   `.  \n" +
+            "    / _ \\      \\ \\  / /    | |__) | /  .-.  \\ \n" + 
+            "   / ___ \\      > `' <     |  __ /  | |   | | \n" +
+            " _/ /   \ \\_  _/ /'`\\ \\_  _| |  \\ \\_\\  `-'  / \n" +
+            "|____| |____||____||____||____| |___|`.___.'  \n" +
+            "Heya!]\n\nType [[b;#44D544;]help] if you dont know what to do next.\n",
+        prompt: function(p){
+            var path = '~'
+            if(GitHub.stack.length > 0) {
+                for(i in GitHub.stack) {
+                    path+= '/';
+                    path+= GitHub.stack[i]
+                }
+            }
+            p(e + ":" + path + "$ ");
+        },
+        onBlur: function() {
+            // prevent loosing focus
+            return false;
+        },
+        tabcompletion: true
+    });
+});
